9f287fe9c5e086f29ee89486ac9fb39fb5396c23
Ilyas <ilyasomar8857@gmail.com>
2023-11-09 17:12:38 +0300

    commit inconnu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app=Flask(__name__)
@@ -6,15 +6,17 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return "asc"
+    return render_template('index.html')
 
 
 
-@app.route("/user/<userName>")
-def user(userName):
-    return f"Bonjour {userName}"
+# @app.route("/user/<userName>")
+# def user(userName):
+#     return f"Bonjour {userName}"
 
-
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', name="Contact")
 
 if __name__=='__main__':
     app.run(debug=True)
