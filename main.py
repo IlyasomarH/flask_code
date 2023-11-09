013a8c4b937505e33ce635d7b659eccd6788e7c1
Ilyas <ilyasomar8857@gmail.com>
2023-11-09 18:21:18 +0300

    test template

--- main.py
+++ main.py
@@ -4,19 +4,27 @@ from flask import Flask, render_template
 app=Flask(__name__)
 
 
+
+# route => templates
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name="Acceuil")
 
-
-
-# @app.route("/user/<userName>")
-# def user(userName):
-#     return f"Bonjour {userName}"
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', name="Contact")
+    return render_template('contact.html', name="contact")
+
+
+@app.route('/formation')
+def formation():
+    return render_template('formation.html', name="formation")
+
+
+@app.route('/propos')
+def propos():
+    return render_template('propos.html', name="propos")
 
 if __name__=='__main__':
     app.run(debug=True)
